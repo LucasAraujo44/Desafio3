15feeed69c3eb8490135edd3c04fba9abf9c91c5
Lucas araujo <lucas.araujo.pb@compasso.com.br>
2022-05-13 15:40:53 -0300

    configurando o arquivo index para receber as rotas na pasta 'routes'

--- api/index.js
+++ api/index.js
@@ -1,16 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
-
-app.use(bodyParser.json())// o body parser vai fazer a ligacao
-
 const port = 3000
 
-app.get('/teste', (req, res) => res
-.status(200)
-.send({mensagem: 'Boas vindas a api'
-}))
+routes(app)
 
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))// verificar se está tudo ok com o srv
 
